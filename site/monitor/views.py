770a0ab7762145803608d269c5183d99371d185b
immanuelw <immwa@sas.upenn.edu>
2016-05-11 12:43:57 -0400

    Attempting to only keep functions than can be modified for RTP

--- site/monitor/views.py
+++ site/monitor/views.py
@@ -6,7 +6,6 @@ author | Immanuel Washington
 Functions
 ---------
 db_objs | gathers database objects for use
-time_fix | fixes times for observation input
 page_args | gathers page arguments for use
 page_form | gathers page form info for use
 obs_filter | filters observation query
@@ -26,7 +25,6 @@ import json
 import time
 from datetime import datetime
 from rtp.site.flask_app import monitor_app as app, monitor_db as db
-from rtp.site import misc_utils
 import dbi as rdbi
 from sqlalchemy import func
 import numpy as np
@@ -47,40 +45,7 @@ def db_objs():
     file_table = rdbi.File
 
     return dbi, obs_table, file_table
-
-def time_fix(jdstart, jdend, starttime=None, endtime=None):
-    '''
-    fixes times for observations
-
-    Parameters
-    ----------
-    jdstart | str: starting time of julian date
-    jdend | str: ending time of julian date
-    starttime | str: string of start time --defaults to None
-    endtime | str: string of end time --defaults to None
-
-    Returns
-    -------
-    tuple:
-        float(5): julian date start time
-        float(5): julian date end time
-    '''
-    try:
-        jd_start = round(float(jdstart), 5)
-        jd_end = round(float(jdend), 5)
-    except:
-        jd_start = None
-        jd_end = None
-
-    if jd_start == None:
-        startdatetime = datetime.strptime(starttime, '%Y-%m-%dT%H:%M:%SZ')
-        enddatetime = datetime.strptime(endtime, '%Y-%m-%dT%H:%M:%SZ')
-        start_utc, end_utc = misc_utils.get_jd_from_datetime(startdatetime, enddatetime)
-    else:
-        start_utc, end_utc = jd_start, jd_end
-
-    return start_utc, end_utc
-
+"""
 def page_args():
     '''
     outputs relevant page argument info
@@ -136,7 +101,7 @@ def page_form():
     start_utc, end_utc = time_fix(jdstart, jdend, starttime, endtime)
 
     return start_utc, end_utc, polarization, era_type, host, filetype
-
+"""
 def obs_filter(obs_query, obs_table, start_utc, end_utc, polarization, era_type):
     '''
     filters observation query
@@ -196,8 +161,6 @@ def index():
     -------
     html: index
     '''
-    polarization_dropdown, era_type_dropdown, host_dropdown, filetype_dropdown = misc_utils.get_dropdowns()
-
     start_utc, end_utc, polarization, era_type, host, filetype = page_args()
 
     dbi, obs_table, file_table = db_objs()
@@ -230,8 +193,6 @@ def index():
             j_day_counts = [0] * len(days)
 
     return render_template('index.html',
-                            polarization_dropdown=polarization_dropdown, era_type_dropdown=era_type_dropdown,
-                            host_dropdown=host_dropdown, filetype_dropdown=filetype_dropdown,
                             start_utc=start_utc, end_utc=end_utc,
                             polarization=polarization, d_pol=polarization,
                             era_type=era_type, d_et=era_type,
@@ -334,27 +295,6 @@ def obs_table():
                             start_time=start_utc, end_time=end_utc,
                             polarization=polarization, era_type=era_type)
 
-@app.route('/save_obs', methods = ['GET'])
-def save_obs():
-    '''
-    saves observations as json
-
-    Returns
-    -------
-    html: json file
-    '''
-    start_utc, end_utc, polarization, era_type, host, filetype = page_args()
-
-    with dbi.session_scope() as s:
-        obs_query = s.query(obs_table)
-        obs_query = obs_filter(obs_query, obs_table, start_utc, end_utc, polarization, era_type)
-        obs_query = obs_query.order_by(obs_table.julian_date.asc()).all()
-
-        entry_list = [obs.to_dict() for obs in obs_query]
-
-    return Response(response=json.dumps(entry_list, sort_keys=True, indent=4, default=ppdata.decimal_default),
-                    status=200, mimetype='application/json', headers={'Content-Disposition': 'attachment; filename=obs.json'})
-
 @app.route('/file_table', methods = ['GET', 'POST'])
 def file_table():
     '''
@@ -381,64 +321,6 @@ def file_table():
                             log_list=log_list, output_vars=output_vars,
                             start_time=start_utc, end_time=end_utc,
                             host=host, filetype=filetype)
-
-@app.route('/save_files', methods = ['GET'])
-def save_files():
-    '''
-    saves file metadata as json
-
-    Returns
-    -------
-    html: json file
-    '''
-    start_utc, end_utc, polarization, era_type, host, filetype = page_args()
-
-    dbi, obs_table, file_table = db_objs()
-    with dbi.session_scope() as s:
-        file_query = s.query(file_table).join(obs_table)
-        file_query = obs_filter(file_query, obs_table, start_utc, end_utc, polarization, era_type)
-        file_query = file_filter(file_query, file_table, host, filetype)
-
-        entry_list = [rtp_file.to_dict() for rtp_file in file_query.order_by(obs_table.time_start.asc()).all()]
-
-    return Response(response=json.dumps(entry_list, sort_keys=True, indent=4, default=ppdata.decimal_default),
-                    status=200, mimetype='application/json', headers={'Content-Disposition': 'attachment; filename=file.json'})
-
-@app.route('/data_summary_table', methods=['POST'])
-def data_summary_table():
-    '''
-    summary of data in main databases
-
-    Returns
-    -------
-    html: data summary table
-    '''
-    start_utc, end_utc, polarization, era_type, host, filetype = page_form()
-
-    pol_strs, era_type_strs, host_strs, filetype_strs = misc_utils.get_set_strings()
-    file_map = {host_str: {filetype_str: {'file_count': 0} for filetype_str in filetype_strs} for host_str in host_strs}
-
-    dbi, obs_table, file_table = db_objs()
-    with dbi.session_scope() as s:
-        file_query = s.query(file_table.host, file_table.filetype, func.count(file_table)).group_by(file_table.host, file_table.filetype).all()
-        for host, filetype, count in file_query:
-            file_map[host][filetype].update({'file_count': count})
-
-    all_file_strs = host_strs + filetype_strs
-    file_total = {all_file_str: {'count': 0} for all_file_str in all_file_strs}
-
-    for host in host_strs:
-        for filetype in filetype_strs:
-            file_count = file_map[host][filetype]['file_count']
-            file_total[filetype]['count'] += file_count
-            file_total[host]['count'] += file_count
-
-    no_files = {filetype: {'file_count': 0} for filetype in filetype_strs}
-    host_strs = tuple(host for host, filetype_dict in file_map.items() if filetype_dict != no_files)
-
-    return render_template('data_summary_table.html',
-                            host_strs=host_strs, filetype_strs=filetype_strs,
-                            file_map=file_map, file_total=file_total)
 
 @app.route('/day_summary_table', methods=['POST'])
 def day_summary_table():
