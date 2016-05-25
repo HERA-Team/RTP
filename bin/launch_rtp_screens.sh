16c4700f1f0c645b03f78ff9e4a00ee57b7981ac
HERA Observer <obs@qmaster>
2016-05-25 19:28:32 +0200

    add in db and monitoring scripts

---
+++ bin/launch_rtp_screens.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+SESSION=RTP2
+
+# Refuse to run if the named session already exists. I'm not
+# aware of a better way to do this.
+
+if screen -ls |grep .$SESSION >/dev/null ; then
+    echo >&2 "error: a screen session named $SESSION already exists"
+    exit 1
+fi
+
+cd ~/src/RTP
+
+screen -d -m -S $SESSION
+screen -S $SESSION -X hardstatus alwayslastline '%c %H%=%h%=%-Lw%{= BW}%n%f %t%{-}%+Lw'
+screen -S $SESSION -X caption splitonly
+screen -S $SESSION -p 0 -X title "RTP Client"
+screen -S $SESSION -X screen
+screen -S $SESSION -p 1 -X title "RTP Servers"
+screen -S $SESSION -X screen
+screen -S $SESSION -p 2 -X title "Deployment"
+screen -S $SESSION -X screen
+screen -S $SESSION -p 3 -X title "Monitoring"
+
+newline='
+'
+screen -S $SESSION -p "RTP Client" -X stuff "source activate HERA$newline"
+screen -S $SESSION -p "RTP Client" -X stuff "bin/still.py --config_file=etc/rtp_hera_test1.cfg --client" #$newline"
+screen -S $SESSION -p "RTP Servers" -X stuff "source activate HERA$newline"
+screen -S $SESSION -p "RTP Servers" -X stuff "bin/launch_rtp_servers.sh" #$newline"
+screen -S $SESSION -p "Deployment" -X stuff "source activate HERA$newline"
+screen -S $SESSION -p "Monitoring" -X stuff "source activate HERA$newline"
+
+# The "select" command doesn't seem to work in -X mode so we can't magically
+# switch back to the Client window.
+#screen -S $SESSION -X select "RTP Client"
+echo "Services launched on screen session $SESSION; attach with \"screen -r $SESSION\"."
---
+++ bin/top_rtp_servers.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+SESSION=TOP
+
+tmux new-session -d -s $SESSION
+
+##Window/pane setup
+tmux new-window -t $SESSION
+#tmux split-window -v
+
+#top half
+tmux split-window -h -t ${SESSION}:0.0
+tmux split-window -v -t ${SESSION}:0.0
+tmux split-window -v -t ${SESSION}:0.1
+#tmux select-pane -t 1
+#tmux split-window -h
+
+
+
+##NODE setup
+tmux select-pane -t 0
+tmux send-keys "ssh still1" C-m
+tmux send-keys "top -u obs" C-m
+
+tmux select-pane -t 1
+tmux send-keys "ssh still2" C-m
+tmux send-keys "top -u obs" C-m
+
+tmux select-pane -t 2
+tmux send-keys "ssh still3" C-m
+tmux send-keys "top -u obs" C-m
+
+tmux select-pane -t 3
+tmux send-keys "ssh still4" C-m
+tmux send-keys "top -u obs" C-m
+
+
+
+#tmux select-pane -t 2
+#tmux send-keys "ssh still3" C-m
+#tmux send-keys "source activate HERA" C-m
+#tmux send-keys "~/src/RTP/bin/still.py --server --config_file=/home/obs/src/RTP/etc/rtp_hera_test1.cfg" C-m
+#
+#tmux select-pane -t 3
+#tmux send-keys "ssh still4" C-m
+#tmux send-keys "source activate HERA" C-m
+#tmux send-keys "~/src/RTP/bin/still.py --server --config_file=/home/obs/src/RTP/etc/rtp_hera_test1.cfg" C-m
+
+#tmux select-pane -t 4
+#tmux send-keys "ssh node04" C-m
+#tmux send-keys "source activate PennStill" C-m
+#tmux send-keys "~/githubs/still_workflow/bin/still.py --server --config_file=/home/saulkohn/githubs/still_workflow/etc/still_shredder.cfg" C-m
+#
+#tmux select-pane -t 5
+#tmux send-keys "ssh node05" C-m
+#tmux send-keys "source activate PennStill" C-m
+#tmux send-keys "~/githubs/still_workflow/bin/still.py --server --config_file=/home/saulkohn/githubs/still_workflow/etc/still_shredder.cfg" C-m
+#
+#tmux select-pane -t 6
+#tmux send-keys "ssh node06" C-m
+#tmux send-keys "source activate PennStill" C-m
+#tmux send-keys "~/githubs/still_workflow/bin/still.py --server --config_file=/home/saulkohn/githubs/still_workflow/etc/still_shredder.cfg" C-m
+#
+#tmux select-pane -t 7
+#tmux send-keys "ssh node07" C-m
+#tmux send-keys "source activate PennStill" C-m
+#tmux send-keys "~/githubs/still_workflow/bin/still.py --server --config_file=/home/saulkohn/githubs/still_workflow/etc/still_shredder.cfg" C-m
+#
+#tmux select-pane -t 8
+#tmux send-keys "ssh node08" C-m
+#tmux send-keys "source activate PennStill" C-m
+#tmux send-keys "~/githubs/still_workflow/bin/still.py --server --config_file=/home/saulkohn/githubs/still_workflow/etc/still_shredder.cfg" C-m
+#
+#tmux select-pane -t 9
+#tmux send-keys "ssh node09" C-m
+#tmux send-keys "source activate PennStill" C-m
+#tmux send-keys "~/githubs/still_workflow/bin/still.py --server --config_file=/home/saulkohn/githubs/still_workflow/etc/still_shredder.cfg" C-m
+
+tmux attach -t ${SESSION}
