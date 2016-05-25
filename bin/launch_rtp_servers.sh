2a07a1f7cae38bd74feb45e97a009495619cc032
HERA Observer <obs@qmaster>
2016-05-25 19:02:58 +0200

    update bin/launch_rtp_servers.sh
    updating tmux options

--- bin/launch_rtp_servers.sh
+++ bin/launch_rtp_servers.sh
@@ -6,27 +6,16 @@ tmux new-session -d -s $SESSION
 
 ##Window/pane setup
 tmux new-window -t $SESSION
-tmux split-window -v
+#tmux split-window -v
 
 #top half
 tmux select-pane -t 0
 tmux split-window -h
 tmux select-pane -t 0
-tmux split-window -h
+tmux split-window -v
 tmux select-pane -t 2
-tmux split-window -h
-tmux select-pane -t 3
 tmux split-window -v
 
-#bottom half
-tmux select-pane -t 5
-tmux split-window -h
-tmux select-pane -t 5
-tmux split-window -h
-tmux select-pane -t 7
-tmux split-window -h
-tmux select-window -t 8
-tmux split-window -v
 
 
 ##NODE setup
@@ -40,15 +29,15 @@ tmux send-keys "ssh still2" C-m
 tmux send-keys "source activate HERA" C-m
 tmux send-keys "~/src/RTP/bin/still.py --server --config_file=/home/obs/src/RTP/etc/rtp_hera_test1.cfg" C-m
 
-#tmux select-pane -t 2
-#tmux send-keys "ssh still3" C-m
-#tmux send-keys "source activate HERA" C-m
-#tmux send-keys "~/src/RTP/bin/still.py --server --config_file=/home/obs/src/RTP/etc/rtp_hera_test1.cfg" C-m
-#
-#tmux select-pane -t 3
-#tmux send-keys "ssh still4" C-m
-#tmux send-keys "source activate HERA" C-m
-#tmux send-keys "~/src/RTP/bin/still.py --server --config_file=/home/obs/src/RTP/etc/rtp_hera_test1.cfg" C-m
+tmux select-pane -t 2
+tmux send-keys "ssh still3" C-m
+tmux send-keys "source activate HERA" C-m
+tmux send-keys "~/src/RTP/bin/still.py --server --config_file=/home/obs/src/RTP/etc/rtp_hera_test1.cfg" C-m
+
+tmux select-pane -t 3
+tmux send-keys "ssh still4" C-m
+tmux send-keys "source activate HERA" C-m
+tmux send-keys "~/src/RTP/bin/still.py --server --config_file=/home/obs/src/RTP/etc/rtp_hera_test1.cfg" C-m
 
 #tmux select-pane -t 4
 #tmux send-keys "ssh node04" C-m
