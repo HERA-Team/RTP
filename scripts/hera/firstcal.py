0adb2e75d47a5bc575c6a1823b8f91b174e975fe
SaulAryehKohn <saul.aryeh.kohn@gmail.com>
2016-06-20 11:46:11 -0400

    flexible options for storing metadata in firstcal files

--- scripts/hera/firstcal.py
+++ scripts/hera/firstcal.py
@@ -6,14 +6,11 @@ import sys,optparse
 o = optparse.OptionParser()
 a.scripting.add_standard_options(o,cal=True,pol=True)
 o.add_option('--plot', action='store_true', help='Plot things.')
-o.add_option('--ex_ants', action='store', type='string', default='',
-    help='Exclude these antennas when solving.')
+o.add_option('--ubls', default='', help='Unique baselines to use, separated by commas (ex: 1_4,64_49).')
+o.add_option('--ex_ants', default='', help='Antennas to exclude, separated by commas (ex: 1,4,64,49).')
 opts,args = o.parse_args(sys.argv[1:])
+connection_file=opts.cal
 PLOT=opts.plot
-
-uv = a.miriad.UV(args[0])
-opts.pol=a.miriad.pol2str[uv['pol']]
-del(uv)
 
 def flatten_reds(reds):
     freds = []
@@ -21,118 +18,68 @@ def flatten_reds(reds):
         freds += r
     return freds
 
-def save_gains(s,f,name='fcgains'):
+def save_gains(s,f,pol,filename=None,ubls=None,ex_ants=None):
+    """
+    s: solutions
+    f: frequencies
+    pol: polarization
+    filename: if a specific file was used (instead of many), change output name
+    ubls: unique baselines used to solve for s'
+    ex_ants: antennae excluded to solve for s'
+    """
     s2 = {}
     for k,i in s.iteritems():
         s2[str(k)] = omni.get_phase(f,i)
-    for k,i in s.iteritems():
-        s2['d'+str(k)] = i
-    import sys
-    cmd = sys.argv
-    s2['cmd'] = ' '.join(cmd)
-    n.savez('%s.npz'%name,**s2)
+    if not ubls is None: s2['ubls']=ubls
+    if not ex_ants is None: s2['ex_ants']=ex_ants
+    if not filename is None:
+        outname='%s.fc.npz'%filename
+    else:
+        outname='fcgains.%s.npz'%pol
+    print 'Saving fcgains to %s'%outname
+    n.savez(outname,**s2)
 
 def normalize_data(datadict):
     d = {}
     for key in datadict.keys():
         d[key] = datadict[key]/n.where(n.abs(datadict[key]) == 0., 1., n.abs(datadict[key]))
-    return d
+    return d 
 
-
-
+    
 #hera info assuming a hex of 19 and 128 antennas
 aa = a.cal.get_aa(opts.cal, n.array([.150]))
-bad_ants = [ant for ant in map(int,opts.ex_ants)]
-info = omni.aa_to_info(aa, fcal=True, ex_ants=bad_ants)
+ex_ants = []
+ubls = []
+for a in opts.ex_ants.split(','):
+    try: ex_ants.append(int(a))
+    except: pass
+for bl in opts.ubls.split(','):
+    try:
+        i,j = bl.split('_')
+        ubls.append((int(i),int(j)))
+    except: pass
+print 'Excluding Antennas:',ex_ants
+if len(ubls) != None: print 'Using Unique Baselines:',ubls
+info = omni.aa_to_info(aa, fcal=True, ubls=ubls, ex_ants=ex_ants)
 reds = flatten_reds(info.get_reds())
-print len(reds)
+#redstest = infotest.get_reds()#for plotting 
 
+print 'Number of redundant baselines:',len(reds)
 #Read in data here.
 ant_string =','.join(map(str,info.subsetant))
 bl_string = ','.join(['_'.join(map(str,k)) for k in reds])
 times, data, flags = arp.get_dict_of_uv_data(args, bl_string, opts.pol, verbose=True)
-dataxx = {}
+dataxx = {} #not necessarily xx data inside
 for (i,j) in data.keys():
     dataxx[(i,j)] = data[(i,j)][opts.pol]
-fqs = n.linspace(.1,.2,1024)
+fqs = n.linspace(.1,.2,203) #XXX
 dlys = n.fft.fftshift(n.fft.fftfreq(fqs.size, fqs[1]-fqs[0]))
 
 #gets phase solutions per frequency.
 fc = omni.FirstCal(dataxx,fqs,info)
-#sols = fc.run(tune=True, verbose=True)
-sols = fc.run(tune=True)
-#import IPython; IPython.embed()
-#save solutions
-fname = args[0]
-save_gains(sols,fqs,name=fname)
+sols = fc.run()
 
+#Save solutions
+if len(args)==1: save_gains(sols,fqs, opts.pol, filename=args[0], ubls=ubls, ex_ants=ex_ants)
+else: save_gains(sols,fqs, opts.pol, ubls=ubls, ex_ants=ex_ants) 
 
-if PLOT:
-    #For plotting a subset of baselines
-    infotest = omni.aa_to_info(aa, fcal=True, ubls=[(80,96)],ex_ants=[81])
-    redstest = infotest.get_reds()
-    #########  To look at solutions and it's application to data. If only lookgin for solutions stop here  ##############
-    dataxx_c = {}
-    print dataxx.keys()
-    for (a1,a2) in info.bl_order():
-        if (a1,a2) in dataxx.keys():
-            dataxx_c[(a1,a2)] = dataxx[(a1,a2)]*omni.get_phase(fqs,sols[a1])*n.conj(omni.get_phase(fqs,sols[a2]))
-            #if a1==43 or a2==43:
-            #    dataxx_c[(a1,a2)]*=n.exp(-2j*n.pi*fqs*n.pi)
-        else:
-            dataxx_c[(a1,a2)] = dataxx[(a2,a1)]*omni.get_phase(fqs,sols[a2])*n.conj(omni.get_phase(fqs,sols[a1]))
-            #if a1==43 or a2==43:
-            #    dataxx_c[(a1,a2)]*=n.exp(-2j*n.pi*fqs*n.pi)
-    #plotting data
-    redbls = []
-    for r in redstest: redbls += r
-    redbls = n.array(redbls)
-    #print redbls.shape
-    #dm = divmod(len(redbls), n.round(n.sqrt(len(redbls))))
-    #nr,nc = int(dm[0]),int(dm[0]+n.ceil(float(dm[1])/dm[0]))
-    #fig,ax = p.subplots(nrows=nr,ncols=nc,figsize=(14,10))
-    #for i,bl in enumerate(redbls):
-    #    bl = (bl[0],bl[1])
-    #    try:
-    #        waterfall(dataxx[bl], ax[divmod(i,nc)], mode='phs')
-    #        ax[divmod(i,nc)].set_title('%d,%d'%(bl))
-    #    except(KeyError):
-    #        waterfall(dataxx[bl[::-1]], ax[divmod(i,nc)], mode='phs')
-    #        ax[divmod(i,nc)].set_title('%d,%d'%(bl[::-1]), color='m')
-    #fig.subplots_adjust(hspace=.5)
-
-    if PLOT:
-        for k, bl in enumerate(redbls):
-            bl = tuple(bl)
-            try:
-                #p.subplot(211); arp.waterfall(dataxx[bl], mode='log',mx=0,drng=3); p.colorbar(shrink=.5)
-                #p.subplot(212); arp.waterfall(dataxx_c[bl], mode='log',mx=0,drng=3); p.colorbar(shrink=.5)
-                p.figure(k)
-                p.subplot(211); arp.waterfall(dataxx[bl], mode='phs'); p.colorbar(shrink=.5)
-                p.subplot(212); arp.waterfall(dataxx_c[bl], mode='phs'); p.colorbar(shrink=.5)
-                p.title('%d_%d'%bl)
-                print sols[bl[0]] - sols[bl[1]]
-                print bl
-            except(KeyError):
-                p.subplot(211); arp.waterfall(dataxx[bl[::-1]], mode='phs'); p.colorbar(shrink=.5)
-                p.subplot(212); arp.waterfall(dataxx_c[bl], mode='phs'); p.colorbar(shrink=.5)
-                p.title('%d_%d'%bl)
-                print bl
-
-        p.show()
-
-
-    data_norm = normalize_data(dataxx_c)
-
-    if PLOT or True:
-        for bl in redbls:
-            bl = tuple(bl)
-            try:
-                print data_norm[bl].shape
-                p.subplot(111); arp.waterfall(n.fft.fftshift(arp.clean_transform(data_norm[bl]),axes=1),extent=(dlys[0],dlys[-1],0,len(redbls))); p.colorbar()
-                p.xlim(-50,50)
-                p.title('%d,%d'%bl)
-            except(KeyError):
-                print 'Key Error on', bl
-
-            p.show()
