2350f42d131f16cb8f8c21f2d6a0406cb50d0360
Peter Williams <peter@newton.cx>
2016-12-09 10:37:54 -0500

    scripts/hera: clean up vestigial "site" terminology and shell-script technique

--- scripts/hera/do_ADD_LIBRARIAN_FIRSTCAL.sh
+++ scripts/hera/do_ADD_LIBRARIAN_FIRSTCAL.sh
@@ -1,12 +1,15 @@
 #! /bin/bash
 set -e
-sitename=${1}
-librarian_path=${3}
-f=$(basename ${2} uvc)
-#only the hexes get firstcal'd
+
+conn_name=$1
+librarian_path=$3
+f=$(basename $2 uvc)
+
+# Only the hexes get firstcal'd:
+
 for ext in HH PH ; do
-    local_file=${f}$ext.uvc.fc.npz
-    librarian_file=${f}$ext.uvc.firstcal.npz
-    echo upload_to_librarian.py ${sitename} ${local_file} ${librarian_path}/${librarian_file}
-    upload_to_librarian.py ${sitename} ${local_file} ${librarian_path}/${librarian_file}
+    local_file="$f$ext.uvc.fc.npz"
+    librarian_file="$f$ext.uvc.firstcal.npz"
+    echo upload_to_librarian.py $conn_name $local_file $librarian_path/$librarian_file
+    upload_to_librarian.py $conn_name $local_file $librarian_path/$librarian_file
 done
--- scripts/hera/do_ADD_LIBRARIAN_SUBARRAY.sh
+++ scripts/hera/do_ADD_LIBRARIAN_SUBARRAY.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 set -e
-sitename=${1}
-librarian_path=${3}
-f=$(basename ${2} uvc)
-for ext in HH PH PI PP
-    do 
-        FILE=${f}${ext}.uvc
-        echo upload_to_librarian.py ${sitename} ${FILE} ${librarian_path}/${FILE}
-        upload_to_librarian.py ${sitename} ${FILE} ${librarian_path}/${FILE}
-done
 
+conn_name=$1
+librarian_path=$3
+f=$(basename $2 uvc)
+
+for ext in HH PH PI PP ; do
+    FILE="$f$ext.uvc"
+    echo upload_to_librarian.py $conn_name $FILE $librarian_path/$FILE
+    upload_to_librarian.py $conn_name $FILE $librarian_path/$FILE
+done
