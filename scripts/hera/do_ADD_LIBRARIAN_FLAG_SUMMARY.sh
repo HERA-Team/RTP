a795093a3397ec63d328dee81a44da77d971d5d5
Paul La Plante <plaplant@sas.upenn.edu>
2017-08-23 10:38:37 -0400

    Change name of pipeline step

---
+++ scripts/hera/do_ADD_LIBRARIAN_FLAG_SUMMARY.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+set -e
+
+# import common functions
+source _common.sh
+
+conn=${1}
+store_path=${2}
+basename=${3}
+
+fn=$(basename ${basename} uv)
+
+summary_f=`echo ${fn}HH.uv.flag_summary.npz`
+total_path=`echo ${store_path}/${summary_f}`
+echo upload_to_librarian.py ${conn} ${summary_f} ${total_path}
+upload_to_librarian.py ${conn} ${summary_f} ${total_path}
