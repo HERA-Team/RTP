ea70a06a1e2f989d9bc855c92875828640e346cf
HERA Observer <obs@qmaster>
2017-09-27 15:03:03 +0000

    Tweak workflow and script specifics

--- scripts/hera/do_PLOTAUTOS.sh
+++ scripts/hera/do_PLOTAUTOS.sh
@@ -7,14 +7,16 @@ source _common.sh
 # get basename
 fn=$(basename $1 uv)
 
-# define polarizations
+# define polarizations of interest
 pol1="xx"
+pol2="yy"
 
 # only run for first thread
 if is_same_pol $fn $pol1; then
-    # make wildcard filename
-    fn_wc=$(replace_pol $fn "??")
+    # make desired filenames
+    fn_xx=$(replace_pol $fn $pol1)
+    fn_yy=$(replace_pol $fn $pol2)
 
-    echo auto_view.py ${fn_wc}uv
-    auto_view.py ${fn_wc}uv
+    echo auto_view.py ${fn_xx}uv ${fn_yy}uv
+    auto_view.py ${fn_xx}uv ${fn_yy}uv
 fi
