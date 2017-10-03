05e6133fdd4d3864f81ede5dd9c29ee7d903e5b7
Paul La Plante <plaplant@sas.upenn.edu>
2017-10-03 10:07:17 -0700

    Update XRFI step in H1C pipeline

--- scripts/hera/do_ADD_LIBRARIAN_XRFI.sh
+++ scripts/hera/do_ADD_LIBRARIAN_XRFI.sh
@@ -8,9 +8,12 @@ conn=${1}
 store_path=${2}
 basename=${3}
 
+pol1="xx"
+
 # get base file name
 bn=$(basename ${basename} uv)
 
+# upload flagged visibility file
 filename=`echo ${bn}HH.uvOR`
 total_path=`echo ${store_path}/${filename}`
 echo upload_to_librarian.py ${conn} ${filename} ${total_path}
@@ -21,3 +24,28 @@ filename=`echo ${bn}HH.uvO.flag_summary.npz`
 total_path=`echo ${store_path}/${filename}`
 echo upload_to_librarian.py ${conn} ${filename} ${total_path}
 upload_to_librarian.py ${conn} ${filename} ${total_path}
+
+# add raw visibility waterfalls
+filename=`echo ${bn}HH.uvO.flags.npz`
+total_path=`echo ${store_path}/${filename}`
+echo upload_to_librarian.py ${conn} ${filename} ${total_path}
+upload_to_librarian.py ${conn} ${filename} ${total_path}
+
+if is_same_pol ${bn} ${pol1}; then
+    # add extra waterfalls
+    nopol_base=$(remove_pol ${bn})
+    vis_wf=`echo ${nopol_base}HH.uv.vis.uvfits.flags.npz`
+    chi_wf=`echo ${nopol_base}HH.uv.omni.calfits.x.flags.npz`
+    g_wf=`echo ${nopol_base}HH.uv.omni.calfits.g.flags.npz`
+
+    # upload to librarian
+    total_path=`echo ${store_path}/${vis_wf}`
+    echo upload_to_librarian.py ${conn} ${vis_wf} ${total_path}
+    upload_to_librarian.py ${conn} ${vis_wf} ${total_path}
+    total_path=`echo ${store_path}/${chi_wf}`
+    echo upload_to_librarian.py ${conn} ${chi_wf} ${total_path}
+    upload_to_librarian.py ${conn} ${chi_wf} ${total_path}
+    total_path=`echo ${store_path}/${g_wf}`
+    echo upload_to_librarian.py ${conn} ${g_wf} ${total_path}
+    upload_to_librarian.py ${conn} ${g_wf} ${total_path}
+fi
--- scripts/hera/do_CLEAN_XRFI.sh
+++ scripts/hera/do_CLEAN_XRFI.sh
@@ -4,8 +4,21 @@ set -e
 # get common functions
 source _common.sh
 
+# define polarization
+pol1="xx"
+
 basename=$(basename $1 uv)
 echo rm -rf ${basename}HH.uvOR
 rm -rf ${basename}HH.uvOR
 echo rm -rf ${basename}HH.uvO.flag_summary.npz
 rm -rf ${basename}HH.uvO.flag_summary.npz
+echo rm -rf ${basename}HH.uvO.flags.npz
+rm -rf ${basename}HH.uvO.flags.npz
+if is_same_pol ${basename} ${pol1}; then
+    echo rm -rf ${basename}HH.uv.vis.uvfits.flags.npz
+    rm -rf ${basename}HH.uv.vis.uvfits.flags.npz
+    echo rm -rf ${basename}HH.uv.omni.calfits.x.flags.npz
+    rm -rf ${basename}HH.uv.omni.calfits.x.flags.npz
+    echo rm -rf ${basename}HH.uv.omni.calfits.g.flags.npz
+    rm -rf ${basename}HH.uv.omni.calfits.g.flags.npz
+fi
